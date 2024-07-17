57de75a04faa964111f2d9b19837d9162733ce8f
Noxy 3 <thatguynoxxie@gmail.com>
2024-07-17 15:50:28 +0700

    ok

---
+++ Elementary_System_Pro/Lab-3/Lab3_1/target/debug/deps/Lab3_1-18527d309760f7f2.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-3\Lab3_1\target\debug\deps\libLab3_1-18527d309760f7f2.rmeta: src/main.rs
+
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-3\Lab3_1\target\debug\deps\Lab3_1-18527d309760f7f2.d: src/main.rs
+
+src/main.rs:
---
+++ Elementary_System_Pro/Lab-3/Lab3_1/target/debug/deps/Lab3_1-6f16e3be2e433290.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-3\Lab3_1\target\debug\deps\libLab3_1-6f16e3be2e433290.rmeta: src/main.rs
+
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-3\Lab3_1\target\debug\deps\Lab3_1-6f16e3be2e433290.d: src/main.rs
+
+src/main.rs:
