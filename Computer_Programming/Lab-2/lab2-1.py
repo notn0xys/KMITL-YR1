ffe0527db9d828f17fffa0fc5bf6eb41ba185752
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-04 01:19:01 +0700

    moved

--- Computer_Programming/Lab-2/lab2-1.py
+++
@@ -1,7 +0,0 @@
-w = 2
-x =3
-y= 4
-z= 5
-
-result = pow(x,3) + (y * (z/2)) - w
-print(result)
---
+++ Computer_Programming/Lab/Lab-2/lab2-2.py
@@ -0,0 +1,10 @@
+import math
+
+r = int(input("Enter Raduis: "))
+l = int(input("Enter Lenght: "))
+
+area = math.pi * r * r
+vol = area * l
+
+print(area)
+print(vol)
