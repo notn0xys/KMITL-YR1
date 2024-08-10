6a5702b5cd4767f92b9d7ee74dbb603825aa71c1
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-10 16:59:25 +0700

    finished 1st

--- Computer_Programming/HW/HW4/HW4-1.py
+++ Computer_Programming/HW/HW4/HW4-1.py
@@ -8,35 +8,45 @@ y2 = float(input("Enter y2: "))
 print("Point 3")
 x3 = float(input("Enter x3: "))
 y3 = float(input("Enter y3: "))
-slope = (y3-y1)/(x3-x1)
-c = y1 - (slope * x1)
-
-if y2 == (slope * x2) + c:
-    print("point 2 is on the line between point 1 and 3")
-else:
-    if slope > 0:
-        if y2 > (slope * x2) + c:
-            print("Point 2 is on the left")
-        else:
-            print("Point 2 is on the Right")
-    elif slope < 0:
-        if y2 > (slope * x2) + c:
-            print("Point 2 is on the Right")
-        else:
-            print("Point 2 is on the Left")
+if x3 - x1 != 0:
+    slope = (y3-y1)/(x3-x1)
+    c = y1 - (slope * x1)
+    if y2 == (slope * x2) + c:
+        print("point 2 is on the line between point 1 and 3")
     else:
-        if y2 > y1:
-            print("Point 2 is on the left")
+        if slope > 0:
+            if y2 > (slope * x2) + c:
+                print("Point 2 is on the left")
+            else:
+                print("Point 2 is on the Right")
+        elif slope < 0:
+            if y2 > (slope * x2) + c:
+                print("Point 2 is on the Right")
+            else:
+                print("Point 2 is on the Left")
         else:
-            print("Point 2 is on the Right")
+            if y2 > y1:
+                print("Point 2 is on the left")
+            else:
+                print("Point 2 is on the Right")
+else:
+    if x2 > x3:
+        print("Point 2 is on the right")
+    else:
+        print("Point 2 is on the left")
+
 
 penup()
 goto(x1,y1)
-write(x1,y1)
+p = pos()
+write(p)
 pendown()
 goto(x3,y3)
-write(x3,y3)
+p = pos()
+write(p)
 penup()
 goto(x2,y2)
-write(x1,y1)
+pendown()
+p = pos()
+write(p)
 done()
