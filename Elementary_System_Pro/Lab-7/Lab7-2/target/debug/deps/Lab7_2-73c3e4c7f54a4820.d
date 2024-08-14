c77509b4e84a2dd5066df0aad0340e462988316c
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-14 17:29:04 +0700

    wrong folder nest

---
+++ Elementary_System_Pro/Lab-7/Lab7-2/target/debug/deps/Lab7_2-73c3e4c7f54a4820.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-7\Lab7-2\target\debug\deps\libLab7_2-73c3e4c7f54a4820.rmeta: src/main.rs
+
+C:\Users\user\Documents\GitHub\KMITL-YR1\Elementary_System_Pro\Lab-7\Lab7-2\target\debug\deps\Lab7_2-73c3e4c7f54a4820.d: src/main.rs
+
+src/main.rs:
