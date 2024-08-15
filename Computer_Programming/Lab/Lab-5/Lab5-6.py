0c9f9a72921b62ee22b83462ac18e04964d4e7ea
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-15 18:27:42 +0700

    done

--- Computer_Programming/Lab/Lab-5/Lab5-6.py
+++ Computer_Programming/Lab/Lab-5/Lab5-6.py
@@ -1,3 +1,4 @@
+import math
 while True:
     n = input("Enter an Integer: ")
     if n.isdigit():
@@ -5,4 +6,13 @@ while True:
         if n > 0:
             break
     print("Wrong input Try agian")
-
+peak = math.ceil(n / 2)
+unpeak = math.floor(n / 2)
+for i in range(peak):
+    for j in range(i + 1):
+        print(2 ** (i - j), end="")
+    print("")
+for i in range(unpeak):
+    for j in range(0, unpeak - i):
+        print(2 ** ((unpeak - i) - (j + 1)), end="")
+    print("")
---
+++ Computer_Programming/Lab/Lab-5/meow.py
@@ -0,0 +1,4 @@
+import math
+n = int(input(" "))
+a = math.ceil(n / 2)
+print(a)
