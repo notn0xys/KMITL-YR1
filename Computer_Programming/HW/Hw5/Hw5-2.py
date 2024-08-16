397d58e43068fd41f13815594f063780da1d0079
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-16 17:56:49 +0700

    okie

--- Computer_Programming/HW/Hw5/Hw5-2.py
+++ Computer_Programming/HW/Hw5/Hw5-2.py
@@ -3,11 +3,16 @@ i = 1
 j = 0
 k = 0
 sq = 0
-lenght  = 30
-width = 25
+lenght  = 35
+width = 20
+left(90)
+penup()
+forward(300)
+right(90)
+pendown()
 while i <= 12:
     j = 0
-    while j < 2:
+    while j < 7:
         if j == 0:
             forward(lenght * 7)
             right(90)
@@ -21,33 +26,35 @@ while i <= 12:
             right(90)
             penup()
             goto(x1 + 10 , y1)
-            write("Month " + str(i) , font=("Arial", 10, "normal"))
+            write("Month#" + str(i) , font=("Arial", 10, "normal"))
             backward(x1 + 10)
             right(90)
             pendown()
             forward(width)
             left(90)
         else:
-            k = 0
-            while k < 6:
-                m = 0
-                while m < 7:
-                    l = 0
-                    while l < 2:
-                        forward(lenght)
-                        left(90)
-                        forward(width)
-                        left(90)
-                        l += 1
+            m = 0
+            while m < 7:
+                l = 0
+                while l < 2:
                     forward(lenght)
-                    m += 1
-                k  += 1
-                backward(lenght * 7)
-                right(90)
+                    left(90)
+                    forward(width)
+                    left(90)
+                    l += 1
+                forward(lenght)
+                m += 1
+            k  += 1
+            backward(lenght * 7)
+            right(90)
+            if j != 6:
                 forward(width)
                 left(90)
         j += 1
-    forward(100)
+    penup()
+    forward(20)
+    left(90)
+    pendown()
     i += 1
 
 done()
