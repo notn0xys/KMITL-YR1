0e9209e75cc4729abf25bd417510e53c5b9d970a
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-17 20:05:47 +0700

    finished q 2

--- Computer_Programming/HW/Hw5/Hw5-2.py
+++ Computer_Programming/HW/Hw5/Hw5-2.py
@@ -3,35 +3,104 @@ i = 1
 j = 0
 k = 0
 sq = 0
+temp_days = 1
+counter = 0
+last_date = 2
+amount_of_days = 0
+list_of_days = ["Su","Mo","Tu","We","Th","Fr","Sa"]
 lenght  = 35
 width = 20
 left(90)
 penup()
-forward(300)
+forward(350)
 right(90)
+meow = pos()
+meow1 , meow2 = meow
 pendown()
 while i <= 12:
+    if i == 5:
+        penup()
+        goto(meow1 - (lenght * 7) - 25 ,meow2)
+        pendown()
+    if i == 9:
+        penup()
+        goto(meow1 - ((lenght * 7) - 25 ) * 2,meow2)
+        pendown()
+    if i == 3 or i == 6:
+        extra = 8
+    else:
+        extra = 7
+    if i == 4 or i == 6 or i == 9 or i == 11:
+        amount_of_days = 30
+    elif i == 2:
+        amount_of_days = 29
+    else: 
+        amount_of_days = 31
     j = 0
-    while j < 7:
+    while j < extra:
         if j == 0:
             forward(lenght * 7)
             right(90)
             forward(width)
             right(90)
             forward(lenght * 7)
-            p = pos()
-            x1 , y1 = p
             right(90)
             forward(width)
             right(90)
             penup()
-            goto(x1 + 10 , y1)
-            write("Month#" + str(i) , font=("Arial", 10, "normal"))
-            backward(x1 + 10)
             right(90)
             pendown()
             forward(width)
             left(90)
+            forward(10)
+            write("Month#" + str(i) , font=("Arial", 10, "normal"))
+            backward(10)
+            right(90)
+            forward(width)
+            left(90)
+
+        elif j == 1:
+            m = 0
+            while m < 7:
+                l = 0
+                while l < 2:
+                    forward(lenght)
+                    left(90)
+                    forward(width)
+                    left(90)
+                    l += 1
+                forward(lenght * (1/4))
+                write(list_of_days[m],font=("Arial", 10, "normal"))
+                forward(lenght * (3/4))
+                m += 1
+            k  += 1
+            backward(lenght * 7)
+            right(90)
+            if j != 6:
+                forward(width)
+                left(90)
+        elif j == 2:
+            m = 0
+            while m < 7:
+                l = 0
+                while l < 2:
+                    forward(lenght)
+                    left(90)
+                    forward(width)
+                    left(90)
+                    l += 1
+                forward(lenght * (1/4))
+                if m >= last_date - 1:
+                    write(temp_days,font=("Arial", 10, "normal"))
+                    temp_days += 1
+                forward(lenght * (3/4))
+                m += 1
+            k  += 1
+            backward(lenght * 7)
+            right(90)
+            if j != 6:
+                forward(width)
+                left(90)
         else:
             m = 0
             while m < 7:
@@ -42,12 +111,24 @@ while i <= 12:
                     forward(width)
                     left(90)
                     l += 1
-                forward(lenght)
+                counter += 1
+                forward(lenght * (1/4))
+                if temp_days <= amount_of_days:
+                    write(temp_days,font=("Arial", 10, "normal"))
+                    if temp_days == amount_of_days:
+                        last_date = counter
+                        print(temp_days) 
+                    temp_days += 1
+                    
+                forward(lenght * (3/4))
                 m += 1
+
             k  += 1
+            counter = 1
+            print(last_date)
             backward(lenght * 7)
             right(90)
-            if j != 6:
+            if j != extra - 1:
                 forward(width)
                 left(90)
         j += 1
@@ -55,6 +136,6 @@ while i <= 12:
     forward(20)
     left(90)
     pendown()
+    temp_days = 1
     i += 1
-
 done()
