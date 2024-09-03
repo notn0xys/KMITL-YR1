20ffdf0f4e470b7a09e3cd498cc852b3ec2ac349
Noxy 3 <thatguynoxxie@gmail.com>
2024-09-03 21:36:00 +0700

    updated

--- Computer_Programming/HW/Hw6/HW6-5.py
+++ Computer_Programming/HW/Hw6/HW6-5.py
@@ -7,7 +7,13 @@ coin10 = 0
 coin5 = 0
 coin2 = 0
 coin1 = 0
-amount = int(input("enter the amount of money you wish to withdraw: "))
+while True:
+    amount = input("enter the amount of money you wish to withdraw: ")
+    if amount.isdigit():
+        amount = int(amount)
+        break
+    print("try agian")
+
 list_of_bills = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
 coin = False
 list_of_var = [bills1k,bills500, bills100, bills50, bills20, coin10, coin5, coin2, coin1]
@@ -15,9 +21,9 @@ for i in range(len(list_of_bills)):
     list_of_var[i] = amount // list_of_bills[i]
     amount -= list_of_var[i] * list_of_bills[i]
 for i in range(len(list_of_bills)):
-    if i > 3:
+    if i > 4:
         coin = True
-    if coin:
+    if coin and list_of_var[i] > 0:
         print(f"{list_of_bills[i]} Coin : {list_of_var[i]}")
-    else:
+    elif coin == False and list_of_var[i] > 0:
         print(f"{list_of_bills[i]} Bills : {list_of_var[i]}")
