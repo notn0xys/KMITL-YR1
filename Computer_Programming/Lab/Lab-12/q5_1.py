99a95a50379ca636a338b0397decdd6dcd8822d2
Noxy 3 <thatguynoxxie@gmail.com>
2024-10-13 10:27:04 +0700

    eee

--- Computer_Programming/Lab/Lab-12/q5_1.py
+++ Computer_Programming/Lab/Lab-12/q5_1.py
@@ -6,7 +6,9 @@ def powerset(s:set):
         meow.add(i)
     return_set.add(frozenset(meow))
     try:
-            for k in s:
+        for k in s:
+            for i in range(len(s) - 2):
+                
                 nyah = s.copy()
                 nyah.remove(k)
                 return_set.add(frozenset(nyah))           
