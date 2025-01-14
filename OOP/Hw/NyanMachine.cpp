6e87b175e4c3c2ad5c9be13b17d138f327890a26
Noxy 3 <thatguynoxxie@gmail.com>
2025-01-14 13:54:16 +0700

    meow

--- OOP/Hw/NyanMachine.cpp
+++ OOP/Hw/NyanMachine.cpp
@@ -5,7 +5,13 @@
 #include <vector>
 
 using namespace std;
-
+struct Money{
+    int bill100;
+    int bill20;
+    int coin10;
+    int coin5;
+    int coin1;
+};
 struct VendingMachineObject {
     int id;
     string name;
@@ -22,8 +28,135 @@ private:
     int Charge = 0;
     int moneybox = 0;
     bool exit = false;
-
-
+    int getint() {
+        int x;
+        cin >> x;
+        while (cin.fail()) {
+            cout << "Invalid input. Please enter a number: ";
+            cin.clear();
+            cin.ignore(256, '\n');
+            cin >> x;
+        }
+        return x;
+    }
+    Money amount_of_bills(int total) {
+        Money money;
+        money.bill100 = total / 100;
+        total = total % 100;
+        money.bill20 = total / 20;
+        total = total % 20;
+        money.coin10 = total / 10;
+        total = total % 10;
+        money.coin5 = total / 5;
+        total = total % 5;
+        money.coin1 = total;
+        return money;
+    }
+    Money get_collectionbox() {
+        Money collection;
+        auto callback = [](void* data, int argc, char** argv, char** azColName) -> int {
+            Money* collection = static_cast<Money*>(data);
+            for (int i = 0; i < argc; i++) {
+                string columnName = azColName[i];
+                if (columnName == "Bill_100") {
+                    collection->bill100 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Bill_20") {
+                    collection->bill20 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_10") {
+                    collection->coin10 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_5") {
+                    collection->coin5 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_1") {
+                    collection->coin1 = argv[i] ? stoi(argv[i]) : 0;
+                }
+            }
+            return 0;
+        };
+        const char* sql = "SELECT Bill_100, Bill_20, Coin_10, Coin_5, Coin_1 FROM collectionbox WHERE id = 1;";
+        if (sqlite3_exec(db, sql, callback, &collection, nullptr) != SQLITE_OK) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        return collection;
+    }
+    void Modify_stock(VendingMachineObject item) {
+        const char* sql = "UPDATE stock_67011177 SET stock = ? WHERE id = ?;";
+        sqlite3_stmt* stmt;
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+        if (rc != SQLITE_OK) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_bind_int(stmt, 1, item.stock);
+        sqlite3_bind_int(stmt, 2, item.id);
+        rc = sqlite3_step(stmt);
+        if (rc != SQLITE_DONE) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_finalize(stmt);
+    }
+    Money Modify_collectionbox(Money collection) {
+        const char* sql = "UPDATE collectionbox SET Bill_100 = ?, Bill_20 = ?, Coin_10 = ?, Coin_5 = ?, Coin_1 = ? WHERE id = 1;";
+        sqlite3_stmt* stmt;
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+        if (rc != SQLITE_OK) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_bind_int(stmt, 1, collection.bill100);
+        sqlite3_bind_int(stmt, 2, collection.bill20);
+        sqlite3_bind_int(stmt, 3, collection.coin10);
+        sqlite3_bind_int(stmt, 4, collection.coin5);
+        sqlite3_bind_int(stmt, 5, collection.coin1);
+        rc = sqlite3_step(stmt);
+        if (rc != SQLITE_DONE) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_finalize(stmt);
+        return collection;
+    }
+    Money get_changebox() {
+        Money change;
+        auto callback = [](void* data, int argc, char** argv, char** azColName) -> int {
+            Money* change = static_cast<Money*>(data);
+            for (int i = 0; i < argc; i++) {
+                string columnName = azColName[i];
+                if (columnName == "Bill_100") {
+                    change->bill100 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Bill_20") {
+                    change->bill20 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_10") {
+                    change->coin10 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_5") {
+                    change->coin5 = argv[i] ? stoi(argv[i]) : 0;
+                } else if (columnName == "Coin_1") {
+                    change->coin1 = argv[i] ? stoi(argv[i]) : 0;
+                }
+            }
+            return 0;
+        };
+        const char* sql = "SELECT Bill_100, Bill_20, Coin_10, Coin_5, Coin_1 FROM changebox WHERE id = 1;";
+        if (sqlite3_exec(db, sql, callback, &change, nullptr) != SQLITE_OK) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        return change;
+    }
+    Money modify_changebox(Money change) {
+        const char* sql = "UPDATE changebox SET Bill_100 = ?, Bill_20 = ?, Coin_10 = ?, Coin_5 = ?, Coin_1 = ? WHERE id = 1;";
+        sqlite3_stmt* stmt;
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+        if (rc != SQLITE_OK) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_bind_int(stmt, 1, change.bill100);
+        sqlite3_bind_int(stmt, 2, change.bill20);
+        sqlite3_bind_int(stmt, 3, change.coin10);
+        sqlite3_bind_int(stmt, 4, change.coin5);
+        sqlite3_bind_int(stmt, 5, change.coin1);
+        rc = sqlite3_step(stmt);
+        if (rc != SQLITE_DONE) {
+            cerr << "SQL error: " << sqlite3_errmsg(db) << endl;
+        }
+        sqlite3_finalize(stmt);
+        return change;
+    }
     vector<VendingMachineObject> get_update_information() {
         vector<VendingMachineObject> stockItems;
 
@@ -54,11 +187,25 @@ private:
 
         return stockItems;
     }
-
+    string displayStock(int amount) {
+        if (amount == 0) {
+            return "Out of stock";
+        } else {
+            return to_string(amount);
+        }
+    }
     void usermode() {
         int items;
         bool found;
         int input;
+        int amounttopay;
+        int inputmoney;
+        int returnmoney;
+        int x;
+        Money change;
+        Money collection;
+        Money ChangeBox;
+        Money CollectionBox;
         while (true) {
             found = false;
             VendingMachineObject targetedItem; 
@@ -66,11 +213,14 @@ private:
             vector<VendingMachineObject> list_of_goods = get_update_information();
             cout << "Items available in the vending machine:\n";
             for (const auto& item : list_of_goods) {
-                cout << "ID: " << item.id << ", Name: " << item.name
-                     << ", Stock: " << item.stock << ", Price: " << item.price << endl;
+                cout << "ID: " << item.id << ", Name: " << item.name << ", Stock: " << displayStock(item.stock) << ", Price: " << item.price << endl;
             }
+            cout << "Press " << list_of_goods.size() + 1 << " to exit" << endl;
             cout << "Choose which item you would like to purchase from the id" << endl;
-            cin >> input;
+            input = getint();
+            if (input == list_of_goods.size() + 1){
+                break;
+            }
             for (const auto& item: list_of_goods) {
                 if (input = item.id) {
                     targetedItem = item;
@@ -85,10 +235,42 @@ private:
             else {
                 cout << "ID: " << targetedItem.id << ", Name: " << targetedItem.name << ", Stock: " << targetedItem.stock << ", Price: " << targetedItem.price << endl;
                 cout << "How many items you wish to buy" << endl;
-                cin >> items;
-                if (items > targetedItem.stock) {
-                    cout << ""
+                items = getint();
+                while (items > targetedItem.stock) {
+                    cout << "Not enough stock, please enter a smaller number" << endl;
+                    items = getint();
                 }
+                amounttopay = items * targetedItem.price;
+                cout << "The total amount you need to pay is " << amounttopay << endl;
+                cout << "Please insert the money" << endl;
+                inputmoney = getint();
+                while (inputmoney < amounttopay) {
+                    cout << "Not enough money, please insert more money" << endl;
+                    x = getint();
+                    inputmoney += x;
+                }
+                cout << "You have inserted " << inputmoney << endl;
+                cout << "your change is" << inputmoney - amounttopay << endl;
+                collection = amount_of_bills(inputmoney);
+                returnmoney = inputmoney - amounttopay;
+                change = amount_of_bills(returnmoney);
+                ChangeBox = get_changebox();
+                CollectionBox = get_collectionbox();
+                CollectionBox.bill100 += collection.bill100;
+                CollectionBox.bill20 += collection.bill20;
+                CollectionBox.coin10 += collection.coin10;
+                CollectionBox.coin5 += collection.coin5;
+                CollectionBox.coin1 += collection.coin1;
+                Modify_collectionbox(CollectionBox);
+                ChangeBox.bill100 -= change.bill100;
+                ChangeBox.bill20 -= change.bill20;
+                ChangeBox.coin10 -= change.coin10;
+                ChangeBox.coin5 -= change.coin5;
+                ChangeBox.coin1 -= change.coin1;
+                modify_changebox(ChangeBox);
+                cout << "You have successfully purchased " << items << " " << targetedItem.name << endl;
+                targetedItem.stock -= items;
+                Modify_stock(targetedItem);
             }
             
 
@@ -99,7 +281,11 @@ private:
         cout << "Admin mode activated.\n";
         return;
     }
-
+    void onexit() {
+        cout << "Exiting the vending machine. Goodbye!\n";
+        sqlite3_close(db);
+        return;
+    }
     int onstart() {
         rc = sqlite3_open("Stock_67011177.db", &db);
         if (rc) {
@@ -173,7 +359,7 @@ public:
             cout << "Please choose from these options: \n";
             cout << "1: User Mode\n2: Admin Mode\n3: Exit\n";
             cout << "Enter your choice: ";
-            cin >> choice;
+            choice = getint();
 
             switch (choice) {
             case 1:
@@ -183,7 +369,7 @@ public:
                 adminmode();
                 break;
             case 3:
-                cout << "Exiting the vending machine. Goodbye!\n";
+                onexit();
                 return;
             default:
                 cout << "Invalid choice. Please try again.\n";
