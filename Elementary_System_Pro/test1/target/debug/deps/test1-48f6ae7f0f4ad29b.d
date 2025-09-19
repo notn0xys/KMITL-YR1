9f18f3298c2b761a39ac0cdab26efe0703f927db
Noxy 3 <thatguynoxxie@gmail.com>
2025-09-20 01:01:22 +0700

    Add initial Rust project 'test1'
    Created a new Rust project named 'test1' with a basic 'Hello, world!' program. Added Cargo.toml, Cargo.lock, and initial source file. Build artifacts and target directory files are included.

---
+++ Elementary_System_Pro/test1/target/debug/deps/test1-48f6ae7f0f4ad29b.d
@@ -0,0 +1,5 @@
+C:\Users\akina\OneDrive\Documents\KMITL-YR1\Elementary_System_Pro\test1\target\debug\deps\libtest1-48f6ae7f0f4ad29b.rmeta: src\main.rs
+
+C:\Users\akina\OneDrive\Documents\KMITL-YR1\Elementary_System_Pro\test1\target\debug\deps\test1-48f6ae7f0f4ad29b.d: src\main.rs
+
+src\main.rs:
---
+++ Elementary_System_Pro/test1/target/debug/deps/test1-9475b2ce763338b2.d
@@ -0,0 +1,5 @@
+C:\Users\akina\OneDrive\Documents\KMITL-YR1\Elementary_System_Pro\test1\target\debug\deps\libtest1-9475b2ce763338b2.rmeta: src\main.rs
+
+C:\Users\akina\OneDrive\Documents\KMITL-YR1\Elementary_System_Pro\test1\target\debug\deps\test1-9475b2ce763338b2.d: src\main.rs
+
+src\main.rs:
